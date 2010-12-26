7165c575be4841d432219e5773b45bb26f00c789
Azharuddin A <azharcs1.6@gmail.com>
2010-12-27 03:48:22 +0530

    commiting some more

---
+++ ex3-3.py
@@ -0,0 +1,15 @@
+# Find something you need to calculate and write and new .py file that does it.
+
+# The total number of headcrabs in Half-Life 2
+
+print "Total number of enemies in Half-Life 2: 9999"
+
+print "standard headcrab: 1135"
+
+print "fast headcrab: 786"
+
+print "poison headcrab: 345"
+
+print "total headcrabs: 1135 + 786 + 345 = ", 1135 + 786 + 345
+
+print "Other enemies:", 9999 - (1135 + 786 + 345) 
--- ex3.py
+++ ex3.py
@@ -1,23 +1,23 @@
 print "I will now count my chickens:" #print the statement to count chickens
 
-print "Hens", 25 + 30 / 6 #counting the Hens
+print "Hens", 25 + 30 / 6 #counting the Hens 
 print "Roosters", 100 - 25 * 3 % 4 #counting the Roosters
 
 print "Now I will count the eggs:" #print the statment to count eggs
 
 print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6 #counting the eggs
 
-print "Is it true that 3 + 2 < 5 - 7?" #
+print "Is it true that 3 + 2 < 5 - 7?" # verifying the equation
 
-print 3 + 2 < 5 - 7
+print 3 + 2 < 5 - 7 # printing the statement to verify True or False
 
-print "What is 3 + 2?", 3 + 2
-print "What is 5 - 7?", 5 - 7
+print "What is 3 + 2?", 3 + 2 # calculating the value
+print "What is 5 - 7?", 5 - 7 # calculating the value
 
-print "Oh, that's why it's False."
+print "Oh, that's why it's False." # statement
 
-print "How about some more."
+print "How about some more." # new verification
 
-print "Is it greater?", 5 > -2
-print "Is it greater or equal?", 5 >= -2
-print "Is it lesser or equal?", 5 <= -2
+print "Is it greater?", 5 > -2 # verifying the statement
+print "Is it greater or equal?", 5 >= -2 # verifying the statement
+print "Is it lesser or equal?", 5 <= -2 # verifying the statement
