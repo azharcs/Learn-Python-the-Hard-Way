ff8ac1f3c09696871f3d942fb95463fecc310901
Azharuddin A <azharcs1.6@gmail.com>
2010-12-27 06:14:24 +0530

    final commit for the day

---
+++ ex5-4.py
@@ -0,0 +1,10 @@
+inches = 75
+centimetres = inches *  2.52
+
+pounds = 160
+kilos = pounds * 0.453
+
+print "%d inches is %0.2f centimetres"  %(inches, centimetres)
+print "%0.2f pounds is %0.2f kilos" %(pounds, kilos)
+
+
---
+++ ex5.py
@@ -0,0 +1,16 @@
+name = 'Azharuddin A'
+age = 25.5 # it's a lie, #facebook scare
+height = 185 #inches
+weight = 74 #kgs
+eyes = 'Black' # color
+teeth = 'White' #color
+hair = 'Black' #color
+
+print "Let's talk about %s." %name
+print "his age is %0.2f" %age
+print "He's %d inches tall" %height
+print "He's %i kgs in weight." %weight
+print "He's got %s eyes and %s hair." %(eyes, hair) 
+print "His teeth's are %s depending on the coffee." %teeth
+
+print "If I add %d, %d and %d, I'd get %d." %(age, height, weight, age + height + weight)
